1bda118be21a3b46439d3499413a189e0841439e
youssef-saaed <youssefsaid2015@hotmail.com>
2023-05-28 18:21:06 +0300

    Merge remote-tracking branch 'origin/master'

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import funcs
+from funcs import *
 
-shape, frames = funcs.getFrames("vid.mp4", "./frames", 0, 95)
-print(shape)
+shape, frames = getFrames("vid.mp4", "./frames", 0, 95)
+
+# img = cv2.cvtColor(frames[40],cv2.COLOR_BGR2RGB)
+# img2 = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+# img2 = cv2.inRange(img2,108,255)
+# img2 = cv2.blur(img2,(3,3))
+# plt.imshow(img2,cmap="gray")
+# plt.show()
